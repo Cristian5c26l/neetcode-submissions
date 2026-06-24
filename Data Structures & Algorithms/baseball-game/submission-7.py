f6b538af6f4500b4f2ea603147a54cfcb048098d
Cristian Hernández <72034530+Cristian5c26l@users.noreply.github.com>
2026-06-24 13:01:24 -0600

    Add: baseball-game - submission-7

---
+++ Data Structures & Algorithms/baseball-game/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+
+        # Optimal solucion
+        # Time complexity: O(n)
+        # Space Complexity: O(n) (due to the worst case of having an operations array with only numbers which leads to an stack of size n)
+
+        scores_stack, points = [], 0
+        for operation in operations:# O(n)
+            if operation not in ["C", "D", "+"]:# O(1): Push (append)
+                scores_stack.append(int(operation))
+                points += scores_stack[-1]# scores_stack[-1] is the top            
+            elif operation == "+":
+                new_score = scores_stack[-1] + scores_stack[-2]
+                scores_stack.append(new_score)
+                points += new_score
+            elif operation == 'D':
+                new_score = 2 * scores_stack[-1]
+                scores_stack.append(new_score)
+                points += new_score
+            elif operation == 'C':# O(1): Pop
+                points -= scores_stack.pop()
+
+        return points
