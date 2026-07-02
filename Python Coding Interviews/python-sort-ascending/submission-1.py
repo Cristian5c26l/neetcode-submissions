16cb9c8e4cafcaf44a69c4aa13338f62cbab9afa
Cristian Hernández <72034530+Cristian5c26l@users.noreply.github.com>
2026-07-02 10:59:29 -0600

    Add: python-sort-ascending - submission-1

---
+++ Python Coding Interviews/python-sort-ascending/submission-1.py
@@ -0,0 +1,44 @@
+from typing import List
+
+"""
+Time and Space Complexity
+
+The time complexity of the .sort() method is 
+O
+(
+n
+l
+o
+g
+n
+)
+O(nlogn), where n is the number of elements in the list.
+
+The space complexity is 
+O
+(
+n
+)
+O(n), where n is the number of elements in the list.
+"""
+
+def sort_words(words: List[str]) -> List[str]:
+    words.sort()# O(nlogn)
+    return words
+
+def sort_numbers(numbers: List[int]) -> List[int]:
+    numbers.sort()
+    return numbers
+
+def sort_decimals(numbers: List[float]) -> List[float]:
+    numbers.sort()
+    return numbers
+
+
+
+# do not modify below this line
+print(sort_words(["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]))
+
+print(sort_numbers([1, 5, 3, 2, 4, 11, 19, 9, 2, 5, 6, 7, 4, 2, 6]))
+
+print(sort_decimals([3.14, 2.82, 6.433, 7.9, 21.555, 21.554]))
