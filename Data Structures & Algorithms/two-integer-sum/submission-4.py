e913e1dfd39c076760ce5df081641fe482f60a5c
Cristian Hernández <72034530+Cristian5c26l@users.noreply.github.com>
2026-07-19 17:35:17 -0600

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Optimal Solution
+        # Time complexity: O(n)
+        # Space complexity: O(n) (O(n) due to the hash could have the size of n, which is the size of the input array)
+
+        nums_hash_map = {}
+        for i in range(len(nums)):
+            difference = target - nums[i]
+            if difference in nums_hash_map:
+                return [nums_hash_map[difference], i]
+            else:
+                nums_hash_map[nums[i]] = i
+
+
+        return []
