7c33131c0de79fa707547fa9fd75282a4301475c
Cristian Hernández <72034530+Cristian5c26l@users.noreply.github.com>
2026-08-03 14:22:59 -0600

    Add: k-closest-points-to-origin - submission-17

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-17.py
@@ -0,0 +1,35 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        def quick_select(points, s, e, k):
+
+            if s >= e:# No more partition/selection
+                return points[:k]
+
+            # Partition
+
+            pivot = points[e]
+            left = s# pointer a
+
+            for i in range(s, e):# i is pointer b
+                x2, y2 = points[i][0], points[i][1] 
+                if (x2 ** 2) + (y2 ** 2) < (pivot[0] ** 2) + (pivot[1] ** 2):
+                    points[i], points[left] = points[left], points[i]
+                    left += 1
+            
+
+            # i has reached e
+            points[left], points[e] = pivot, points[left]
+
+            # At this point, before the pivot value (points[left]) are smaller values (likely unsorted), and after are greater values (likely unsorted). Moreover, pivot is already sorted.
+
+            if left == k - 1:# Finish immediately due there is no need of sorting the values
+                return points[:k]
+
+            # Quick select
+            if left < k - 1: 
+                return quick_select(points, left + 1, e, k)
+            else:# left > k -1:, for example: left = 1, when input is points=[[0,2],[2,2]] and k = 1
+                return quick_select(points, s, left - 1, k)
+
+        return quick_select(points, 0, len(points) - 1, k)
+        
