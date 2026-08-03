020217b3933f5dd96f9d66113d1bd13a398c74e7
Cristian Hernández <72034530+Cristian5c26l@users.noreply.github.com>
2026-08-03 15:49:03 -0600

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.py
@@ -0,0 +1,36 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Optimal Solution
+        # Time Complexity: O(log(m * n))
+        # Space Complexity: O(1)
+        m = len(matrix) - 1# row
+        #n = len(matrix[0])# columns
+        
+        L, R = 0, m
+        row = None
+        while L <= R:
+            mid = (L + R) // 2
+
+            if target > matrix[mid][-1]:
+                L = mid + 1
+            elif target < matrix[mid][0]:
+                R = mid - 1
+            else:
+                row = matrix[mid]
+                break
+
+        if row is None:
+            return False
+
+        L, R = 0, len(row) - 1
+        while L <= R:
+            mid = (L + R) // 2
+
+            if target > row[mid]:
+                L = mid + 1
+            elif target < row[mid]:
+                R = mid - 1
+            else:
+                return True
+
+        return False
