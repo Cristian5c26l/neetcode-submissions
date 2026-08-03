02bcad46dede9a7399842202d16a6e1cb1116fb0
Cristian Hernández <72034530+Cristian5c26l@users.noreply.github.com>
2026-08-03 13:18:43 -0600

    Add: k-closest-points-to-origin - submission-11

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-11.py
@@ -0,0 +1,37 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        def quick_sort(points, s, e, k):
+
+            #if e == k - 1:
+            #    return list(set(points[0:k]))
+            
+            if e - s + 1 <= 1:
+                return points
+
+            pivot = points[e]
+            left = s# pointer a
+
+            for i in range(s, e):# i is pointer b
+                x2, y2 = points[i][0], points[i][1] 
+                if (x2 ** 2) + (y2 ** 2) < (pivot[0] ** 2) + (pivot[1] ** 2):
+                    points[i], points[left] = points[left], points[i]
+                    left += 1
+            
+
+            # i has reached e
+            points[left], points[e] = pivot, points[left]
+
+            # At this point, before the pivot value (points[left]) are smaller values (likely unsorted), and after are greater values (likely unsorted). Moreover, pivot is already sorted.
+
+            if left == k or left + 1 == k:# 1 == 1
+                return points[0:k]
+
+            # Quick select
+            if left + 1 < k: 
+                return quick_sort(points, left + 1, e, k)
+            elif left + 1 > k:
+                return quick_sort(points, s, left - 1, k)
+
+
+        return quick_sort(points, 0, len(points) - 1, k)
+        
